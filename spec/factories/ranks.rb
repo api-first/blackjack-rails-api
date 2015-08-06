52f17b2bdb738e0b75fcac5746787e9f4ec6d66c
Sean Devine <barelyknown@icloud.com>
2015-08-05 23:35:20 -0400

    Update ranks factory to include order attribute.

--- spec/factories/ranks.rb
+++ spec/factories/ranks.rb
@@ -8,33 +8,36 @@ FactoryGirl.define do
         initial n.to_s
         name n.to_s
         values [n]
+        order n - 2
       end
     end
     factory :jack do
       initial "J"
       name "Jack"
       values [10]
+      order 9
     end
     factory :queen do
       initial "Q"
       name "Queen"
       values [10]
+      order 10
     end
     factory :king do
       initial "K"
       name "King"
       values [10]
+      order 11
     end
     factory :ace do
       initial "A"
       name "Ace"
       values [1,11]
+      order 12
     end
 
     initialize_with do
-      Rank.find_or_initialize_by(initial: initial, name: name).tap do |rank|
-        rank.values = values
-      end
+      Rank.find_or_initialize_by(initial: initial, name: name)
     end
   end
 end
