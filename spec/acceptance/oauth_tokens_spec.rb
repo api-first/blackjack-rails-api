6aef651d3e535035751b958cec7390a4b324b3f8
David Stosik <david.stosik+git-noreply@gmail.com>
2015-09-18 02:52:30 +0900

    Update API documentation to describe OAuth authentication with email or username.

--- spec/acceptance/oauth_tokens_spec.rb
+++ spec/acceptance/oauth_tokens_spec.rb
@@ -10,6 +10,53 @@ RSpec.resource "OAuth Tokens" do
     end
 
     let! :user do
+      FactoryGirl.create(:user, username: username, password: password)
+    end
+
+    parameter "grant_type", <<-DESC, required: true
+      The OAuth2 grant type. Must be <code>password</code>.
+    DESC
+
+    let "grant_type" do
+      "password"
+    end
+
+    parameter "username", <<-DESC, required: true
+      The username of the user.
+    DESC
+
+    let "username" do
+      "sean"
+    end
+
+    parameter "password", <<-DESC, required: true
+      The password of the user.
+    DESC
+
+    let "password" do
+      "drowssap"
+    end
+
+    example_request "POST /oauth/token with username" do
+      explanation "Authenticate through OAuth using username."
+      expect(status).to eq 200
+      expect(JSON.parse(response_body)["access_token"]).not_to be_nil
+    end
+
+    after do
+      RspecApiDocumentation.configuration.post_body_formatter = @post_body_formatter
+    end
+  end
+
+  post "/oauth/token" do
+    before do
+      @post_body_formatter = RspecApiDocumentation.configuration.post_body_formatter.dup
+      RspecApiDocumentation.configuration.post_body_formatter = proc do |params|
+        params
+      end
+    end
+
+    let! :user do
       FactoryGirl.create(:user, username: username, password: password, email: email)
     end
 
@@ -21,15 +68,11 @@ RSpec.resource "OAuth Tokens" do
       "password"
     end
 
-    parameter "username", <<-DESC
-      The username of the user.
-    DESC
-
     let "username" do
       "sean"
     end
 
-    parameter "email", <<-DESC
+    parameter "email", <<-DESC, required: true
       The email of the user.
     DESC
 
@@ -45,7 +88,8 @@ RSpec.resource "OAuth Tokens" do
       "drowssap"
     end
 
-    example_request "POST /oauth/token" do
+    example_request "POST /oauth/token with email" do
+      explanation "Authenticate through OAuth using email."
       expect(status).to eq 200
       expect(JSON.parse(response_body)["access_token"]).not_to be_nil
     end
